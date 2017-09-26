42c971e8b826317c8d30672956ba626e6704deb1
Damien <mail@damientimewell.com>
2017-09-26 14:48:10 +0200

    Using state to render cards

--- src/game.js
+++ src/game.js
@@ -1,13 +1,29 @@
 import React from "react"
 import Card from "./card"
 
-const Game = () => (
-  <div className="game">
-    <h1>Memory game</h1>
-    <Card src="/images/dog1.jpeg" />
-    <Card src="/images/dog2.jpeg" />
-    <Card src="/images/dog3.jpeg" />
-  </div>
-)  
+class Game extends React.Component {
+
+  state = {
+    cards: [
+      { src: "/images/dog1.jpeg" },
+      { src: "/images/dog2.jpeg" },
+      { src: "/images/dog3.jpeg" }
+    ]
+  }
+
+  renderCard = (something) => {
+    return <Card src={something.src} />
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <h1>Memory game</h1>
+        {this.state.cards.map(this.renderCard)}
+      </div>
+    )
+  }
+
+}
 
 export default Game
